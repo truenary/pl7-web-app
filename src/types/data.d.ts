8ddcbde76e1a33b62541b76600c9b11cabc013d1
Someone-rahul <156889487+Someone-rahul@users.noreply.github.com>
2024-01-17 15:44:45 +0545

    added: driver type data (#1)
    Co-authored-by: Sagar Adhikari <sagarjungadhikari19@gmail.com>

---
+++ src/types/data.d.ts
@@ -0,0 +1,10 @@
+declare type Driver = {
+    firstName: string;
+    lastName: string;
+    phoneNumber: number;
+    liscenceNumber: string;
+    vehicleNumber: string;
+    vehicleType: string;
+    vehicleModel: string;
+
+}
