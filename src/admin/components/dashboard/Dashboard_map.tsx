ef671b6492ec84b8a97ce5966f126e437dd95123
Sagar Adhikari <sagarjungadhikari19@gmail.com>
2024-01-19 13:21:00 +0545

    jsondb_updated

--- src/admin/components/dashboard/Dashboard_map.tsx
+++ src/admin/components/dashboard/Dashboard_map.tsx
@@ -1,48 +1,41 @@
-import React from 'react'
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
-
-const libraries: ['places'] = ['places'];
+const libraries: ["places"] = ["places"];
 const mapContainerStyle = {
-    width: '79vw',
-    height: '80vh',
+  width: "79vw",
+  height: "80vh",
 };
 const center = {
-    lat: 27.431353305174838, // default latitude
-    lng: 85.03178229329563, // default longitude
+  lat: 27.431353305174838, // default latitude
+  lng: 85.03178229329563, // default longitude
 };
 
 function Dashboard_map() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyCaYa9vMmqtJyBo47znD-vZrNRSVuOKJz4",
+    libraries,
+  });
 
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
 
-    const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: 'AIzaSyCaYa9vMmqtJyBo47znD-vZrNRSVuOKJz4',
-        libraries,
-    });
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
 
-    if (loadError) {
-        return <div>Error loading maps</div>;
-    }
-
-    if (!isLoaded) {
-        return <div>Loading maps</div>;
-    }
-
-
-    return (
-        <div className='my-4'>
-            <GoogleMap
-                mapContainerStyle={mapContainerStyle}
-                zoom={15}
-                center={center}
-            >
-                <Marker position={center} />
-            </GoogleMap>
-        </div>
-
-
-
-    )
+  return (
+    <div className="my-4">
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={15}
+        center={center}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </div>
+  );
 }
 
-export default Dashboard_map
+export default Dashboard_map;
