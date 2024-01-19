32ecd0320e6719f6c65eb4014cf2e96f1222705c
Someone-rahul <156889487+Someone-rahul@users.noreply.github.com>
2024-01-19 12:19:57 +0545

    Master (#3)
    * added: driver type data
    
    * feat: added client side pages and components
    
    * admin-dashboard-updated
    
    ---------
    
    Co-authored-by: Sagar Adhikari <sagarjungadhikari19@gmail.com>

---
+++ src/admin/pages/Admin_Layout.tsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../global/Sidebar';
+
+export default function Admin_Layout() {
+    return (
+        <div className="h-full overflow-hidden ">
+            <main className="flex gap-4">
+                <div className=''>
+                    <Sidebar />
+                </div>
+                <div className="pt-0 px-4 bg-blue-500 mt-0 mx-auto flex-1 flex-col max-h-screen overflow-y-auto">
+                    <Outlet />
+                </div>
+            </main>
+        </div>
+    );
+}
